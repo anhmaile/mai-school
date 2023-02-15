5a0743001a295a9ca9eb0e0eed277d2ec1669553
anhmaile <118566182+anhmaile@users.noreply.github.com>
2023-02-15 15:27:04 -0800

    Add Schedule Table

--- page-schedule.php
+++ page-schedule.php
@@ -23,6 +23,53 @@ get_header();
 
 			get_template_part( 'template-parts/content', 'page' );
 
+			// Solution Reference: https://stackoverflow.com/questions/34989806/acf-loop-repeater-values-with-get-field *Foreach Version
+			?>
+			<div class="table-container">
+			<div class="table-date">
+			<?php
+			$rows = get_field('weekly_course_schedule');
+			if($rows) {
+				echo '<ul>';
+				echo '<h3>Date</h3>';
+				foreach ($rows as $row) {
+					echo '<li>' . $row['date'] . '</li>';
+				}
+				echo '</ul>';
+			}
+			?>
+			</div>
+
+			<div class="table-course">
+			<?php
+			if($rows) {
+				echo '<ul>';
+				echo '<h3>Course</h3>';
+				foreach ($rows as $row) {
+					echo '<li>' . $row['course'] . '</li>';
+				}
+				echo '</ul>';
+			}
+
+			?>
+			</div>
+
+			<div class="table-instructor">
+			<?php
+			if($rows) {
+				echo '<ul>';
+				echo '<h3>Instructor</h3>';
+				foreach ($rows as $row) {
+					echo '<li>' . $row['instructor'] . '</li>';
+				}
+				echo '</ul>';
+			}
+			?>
+			</div>
+			</div>
+
+			<?php
+
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
